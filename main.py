6b72893095c472d5ba0d5c8661e4e756ae5ec7c3
khilling <55695412+khilling@users.noreply.github.com>
2021-04-16 19:16:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,35 @@ class MyWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, resizable=False)
         self.set_default_size(1000, 700)
-        fixed = Gtk.fixed()
-        self.add(fixed)
         self.brush = {'width':10, 'color':(1, 1, 1)}
         self.strokes = []
+        self.written_strokes = []
         self.drawing_init()
+        self.colors = {}
 
 
+    def change_color(self, button, color):
+        self.brush['color'] = color
+    
+    
     def drawing_init(self):
         vbox = Gtk.VBox()
         vbox.set_homogeneous(False)
+        hbox = Gtk.HBox()
+        vbox.pack_start(hbox, True, True, 0)
+        red_button = Gtk.Button(name="r-btn")
+        blue_button = Gtk.Button(name="b-btn")
+        green_button = Gtk.Button(name="g-btn")
+        white_button = Gtk.Button(name="w-btn")
+        hbox.pack_end(red_button, True, True, 0)
+        hbox.pack_end(blue_button, True, True, 0)
+        hbox.pack_end(white_button, True, True, 0)
+        hbox.pack_end(green_button, True, True, 0)
+        
+
         area = Gtk.DrawingArea(name = 'area')
-        area.set_property("width-request", 500)
-        area.set_property("height-request", 500)
+        area.set_property("width-request", 1000)
+        area.set_property("height-request", 600)
         area.connect("draw", self.draw)
         area.connect('motion-notify-event', self.mouse_move)
         area.connect("button-press-event", self.mouse_press)
@@ -29,10 +45,14 @@ class MyWindow(Gtk.Window):
             Gdk.EventMask.POINTER_MOTION_MASK |
             Gdk.EventMask.BUTTON_RELEASE_MASK)
         fixed = Gtk.Fixed()
-        fixed.put(area, 250, 0)
+        fixed.put(area, 0, 0)
         fixed.set_property("width-request", 1000)
         fixed.set_property("height-request", 550)
         vbox.pack_end(fixed, False, False, 0)
+        white_button.connect("clicked", self.change_color, (1, 1, 1))
+        red_button.connect("clicked", self.change_color, (1, 0, 0))
+        blue_button.connect("clicked", self.change_color, (0, 0, 1))
+        green_button.connect("clicked", self.change_color, (0, 1, 0))
         self.add(vbox)
         self.show_all()
     
@@ -40,8 +60,11 @@ class MyWindow(Gtk.Window):
     def draw(self, widget, cr):
         cr.set_source_rgb(0.2, 0.2, 0.2)
         cr.paint()
-        for stroke in self.strokes:
-            cr.set_source_rgb(self.brush['color'][0], self.brush['color'][1], self.brush['color'][2])
+        for i in range(len(self.strokes)):
+            stroke = self.strokes[i]
+            if stroke not in self.written_strokes:
+                self.colors[i] = [self.brush['color'][0], self.brush['color'][1], self.brush['color'][2]]
+            cr.set_source_rgb(self.colors[i][0], self.colors[i][1], self.colors[i][2])
             cr.set_line_width(self.brush['width'])
             cr.set_line_cap(1)
             cr.set_line_join(cairo.LINE_JOIN_ROUND)
@@ -49,10 +72,15 @@ class MyWindow(Gtk.Window):
             for x, y in stroke:
                 cr.line_to(x, y) 
             cr.stroke()
+            self.written_strokes.append(stroke)
 
 
     def mouse_press(self, widget, event):
-        if event.button == Gdk.BUTTON_SECONDARY:
+        if event.button == Gdk.BUTTON_PRIMARY:
+            self.strokes.append(list())
+            self.strokes[-1].append((event.x, event.y))
+            widget.queue_draw()
+        elif event.button == Gdk.BUTTON_SECONDARY:
             self.strokes.clear()
 
 
@@ -81,4 +109,3 @@ def main():
 
 if __name__ == "__main__":    
     main()
-
